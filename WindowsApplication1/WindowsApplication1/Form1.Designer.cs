4768ac63dbff1ac07f2bd4d8900cc604b9414450
Sunshine Eunice Crisostomo <sunshineeunicec@gmail.com>
2016-02-10 16:51:27 +0800

    First commit

---
+++ WindowsApplication1/WindowsApplication1/Form1.Designer.cs
@@ -0,0 +1,224 @@
+﻿namespace WindowsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.DGViewName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.DGViewType = new System.Windows.Forms.DataGridViewComboBoxColumn();
+            this.DGViewWidth = new DataGridViewNumericUpDownElements.DataGridViewNumericUpDownColumn();
+            this.DGViewDecimal = new DataGridViewNumericUpDownElements.DataGridViewNumericUpDownColumn();
+            this.DGViewIndex = new System.Windows.Forms.DataGridViewComboBoxColumn();
+            this.DGViewNull = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.btnOK = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.btnInsert = new System.Windows.Forms.Button();
+            this.btnDel = new System.Windows.Forms.Button();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.DGViewName,
+            this.DGViewType,
+            this.DGViewWidth,
+            this.DGViewDecimal,
+            this.DGViewIndex,
+            this.DGViewNull});
+            this.dataGridView1.Location = new System.Drawing.Point(9, 14);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(674, 150);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // DGViewName
+            // 
+            this.DGViewName.HeaderText = "Name";
+            this.DGViewName.Name = "DGViewName";
+            this.DGViewName.Width = 120;
+            // 
+            // DGViewType
+            // 
+            this.DGViewType.HeaderText = "Type";
+            this.DGViewType.Name = "DGViewType";
+            // 
+            // DGViewWidth
+            // 
+            this.DGViewWidth.HeaderText = "Width";
+            this.DGViewWidth.Maximum = new decimal(new int[] {
+            50,
+            0,
+            0,
+            0});
+            this.DGViewWidth.Name = "DGViewWidth";
+            // 
+            // DGViewDecimal
+            // 
+            this.DGViewDecimal.HeaderText = "Decimal";
+            this.DGViewDecimal.Name = "DGViewDecimal";
+            // 
+            // DGViewIndex
+            // 
+            this.DGViewIndex.HeaderText = "Index";
+            this.DGViewIndex.Name = "DGViewIndex";
+            // 
+            // DGViewNull
+            // 
+            this.DGViewNull.HeaderText = "NULL";
+            this.DGViewNull.Name = "DGViewNull";
+            // 
+            // btnOK
+            // 
+            this.btnOK.Location = new System.Drawing.Point(717, 17);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(75, 27);
+            this.btnOK.TabIndex = 1;
+            this.btnOK.Text = "OK";
+            this.btnOK.UseVisualStyleBackColor = true;
+            this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(717, 46);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 27);
+            this.btnCancel.TabIndex = 2;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // btnInsert
+            // 
+            this.btnInsert.Location = new System.Drawing.Point(717, 98);
+            this.btnInsert.Name = "btnInsert";
+            this.btnInsert.Size = new System.Drawing.Size(75, 27);
+            this.btnInsert.TabIndex = 3;
+            this.btnInsert.Text = "Add";
+            this.btnInsert.UseVisualStyleBackColor = true;
+            this.btnInsert.Click += new System.EventHandler(this.btnInsert_Click);
+            // 
+            // btnDel
+            // 
+            this.btnDel.Location = new System.Drawing.Point(717, 127);
+            this.btnDel.Name = "btnDel";
+            this.btnDel.Size = new System.Drawing.Size(75, 27);
+            this.btnDel.TabIndex = 4;
+            this.btnDel.Text = "Delete";
+            this.btnDel.UseVisualStyleBackColor = true;
+            this.btnDel.Click += new System.EventHandler(this.btnDel_Click);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(5, -1);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(700, 220);
+            this.tabControl1.TabIndex = 5;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.dataGridView1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(692, 194);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Fields";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.dataGridView2);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(692, 194);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Indexes";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(11, 20);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.Size = new System.Drawing.Size(670, 150);
+            this.dataGridView2.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(805, 234);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.btnDel);
+            this.Controls.Add(this.btnInsert);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnOK);
+            this.Name = "Form1";
+            this.Text = "Table Designer";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnOK;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Button btnInsert;
+        private System.Windows.Forms.Button btnDel;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn DGViewName;
+        private System.Windows.Forms.DataGridViewComboBoxColumn DGViewType;
+        private DataGridViewNumericUpDownElements.DataGridViewNumericUpDownColumn DGViewWidth;
+        private DataGridViewNumericUpDownElements.DataGridViewNumericUpDownColumn DGViewDecimal;
+        private System.Windows.Forms.DataGridViewComboBoxColumn DGViewIndex;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn DGViewNull;
+        private System.Windows.Forms.DataGridView dataGridView2;
+    }
+}
---
+++ WindowsApplication1/WindowsApplication1/Form1.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Data;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+
+namespace WindowsApplication1
+{
+    public partial class Form1 : Form 
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            AddAllInputtoDataGridView();
+        }
+        DataTable dt = new DataTable();
+        //int i;
+      
+        public string textName { get; set; }
+        public string Cmbtype { get; set; }
+        public int PWidth { get; set; }
+        public int Decimal { get; set; }
+        public string Index { get; set; }
+        public bool checkboxNull { get; set; }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            foreach (DataGridViewColumn col in dataGridView1.Columns) {
+               dt.Columns.Add(col.Name);
+                col.DataPropertyName = col.Name;
+            }
+
+            DGViewName.MaxInputLength = 10;
+
+            DGViewType.Items.Add("Character");
+            DGViewType.Items.Add("Currency");
+            DGViewType.Items.Add("Numeric");
+            DGViewType.Items.Add("Float");
+            DGViewType.Items.Add("Date");
+            DGViewType.Items.Add("DateTime");
+            DGViewType.Items.Add("Double");
+            DGViewType.Items.Add("Integer");
+            DGViewType.Items.Add("Logical");
+            DGViewType.Items.Add("Memo");
+            DGViewType.Items.Add("General");
+            DGViewType.Items.Add("Character (binary)");
+            DGViewType.Items.Add("Memo (binary)");
+
+            DGViewIndex.Items.Add("None");
+            DGViewIndex.Items.Add("Ascending");
+            DGViewIndex.Items.Add("Descending");
+
+            DataRow drLocal = null;
+            foreach (DataGridViewRow dr in dataGridView1.Rows)
+            {
+                drLocal = dt.NewRow();
+                drLocal["DGViewName"] = dr.Cells["DGViewName"].Value;
+                drLocal["DGViewType"] = dr.Cells["DGViewType"].Value;
+                drLocal["DGViewWidth"] = dr.Cells["DGViewWidth"].Value;
+                drLocal["DGViewDecimal"] = dr.Cells["DGViewDecimal"].Value;
+                drLocal["DGViewIndex"] = dr.Cells["DGViewIndex"].Value;
+                drLocal["DGViewNull"] = dr.Cells["DGViewNull"].Value;  
+                dt.Rows.Add(drLocal);
+                //dataGridView1.DataSource = dt;
+            }
+            //dataGridView1.DataSource = dt;  
+        }
+
+        private void btnInsert_Click(object sender, EventArgs e)
+        {
+                AddAllInputtoDataGridView(); 
+                dataGridView1.ReadOnly  =   false;
+                dt.Rows.Add("NewFld","Character","10","0","","false");
+                dataGridView1.DataSource = dt;
+        }
+
+        private void btnDel_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                dt.Rows.RemoveAt(dataGridView1.CurrentCell.RowIndex);
+                dataGridView1.DataSource = dt;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.Rows.Count == 0)
+            {
+                Application.Exit();
+            }
+            else
+            {
+                DialogResult resultCancel = MessageBox.Show("Discard Structure Changes?", "Name", MessageBoxButtons.YesNo);
+                if ((resultCancel == DialogResult.Yes && dataGridView1.Rows.Count != 0))
+                {
+                    Application.Exit();
+                }
+            }
+        }
+
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+            StringBuilder s = new StringBuilder();
+            foreach (DataGridViewRow row in dataGridView1.Rows)
+            {
+                for (int i = 0; i < dataGridView1.Columns.Count; i++)
+                {
+                    if (row.IsNewRow)
+                        continue;
+
+                    string header = dataGridView1.Columns[i].HeaderText;
+                    string cellText = row.Cells[i].Value.ToString();
+                    // MessageBox.Show(header +": "+cellText);
+                    s.AppendLine(header + ": " + cellText);
+                }
+            }
+            MessageBox.Show(s.ToString());         
+        }
+
+        private void AddAllInputtoDataGridView() {
+            DataRow drLocal = null;
+            foreach (DataGridViewRow dr in dataGridView1.Rows)
+            {
+                drLocal = dt.NewRow();
+                drLocal["DGViewName"] = dr.Cells["DGViewName"].Value;
+                drLocal["DGViewType"] = dr.Cells["DGViewType"].Value;
+                drLocal["DGViewWidth"] = dr.Cells["DGViewWidth"].Value;
+                drLocal["DGViewDecimal"] = dr.Cells["DGViewDecimal"].Value;
+                drLocal["DGViewIndex"] = dr.Cells["DGViewIndex"].Value;
+                drLocal["DGViewNull"] = dr.Cells["DGViewNull"].Value;
+                dt.Rows.Add(drLocal);
+                //dataGridView1.DataSource = dt;
+            }
+            return dt;
+        }
+    } 
+ }
